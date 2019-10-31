4811cc22ba531d33242d85119f03020eb17025ef
wcabezas <34169621+wcabezas@users.noreply.github.com>
2019-10-31 14:24:10 -0300

    Se agregaron los test de la clase Club

---
+++ tests/testsUnitarios/TestClub.java
@@ -0,0 +1,32 @@
+package testsUnitarios;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import lugaresDeInteres.Club;
+import modelo.Pais;
+import modelo.Villano;
+
+class TestClub {
+
+	@Test
+	public void obtenerVillanoCuandoEstaEnClub() {
+		Club club = new Club();
+		Villano villano = Mockito.mock(Villano.class);
+		club.setVillanoEnElLugar(villano);
+		assertEquals(villano,club.getVillanoEnElLugar());
+	}
+	
+	@Test
+	public void obtengoPaisDeOrigenDelClub() {
+		Club club = new Club();
+		Pais australia = Mockito.mock(Pais.class);
+		club.setPaisDeOrigen(australia);
+		assertEquals(australia,club.getPaisDeOrigen());
+	}
+		
+	}
+
+}
